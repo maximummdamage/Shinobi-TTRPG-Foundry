ca8e0f579159fa10423610ddc11bfb29c809cffd
madupree <madupree@iastate.edu>
2022-09-12 21:50:58 -0500

    experimenting with resources on character sheet

--- module/documents/actor.mjs
+++ module/documents/actor.mjs
@@ -1,4 +1,39 @@
-// SRPG actor class
+// Extends base actor document
 export class SRPGActor extends Actor {
 
+    /**
+     * Prepare Data for the actor. calling super does the following: data reset,
+     * prepareBaseData(), prepareEmbeddedDocuments(), prepareDerivedData()
+     * @override
+     */
+    prepareData() {
+        super.prepareData();
+    }
+
+    /** @override */
+    prepareBaseData() {
+
+    }
+
+    /**
+     * handle most calculated/derived data in this step,
+     * should not exist in template.json (maxhp, maxchk),
+     * and is availible both in and out of character sheet
+     * @override
+     */
+    prepareDerivedData() {
+        if (this.type == "shinobi") {
+            this._prepareShinobiData(this.data);
+        } else if (this.type == "mook") {
+            this._prepareNpcData(this.data);
+        }
+    }
+
+    _prepareShinobiData(data) {
+        console.log("SHINOBI");
+    }
+
+    _prepareNpcData(data) {
+        console.log("MOOK");
+    }
 }
