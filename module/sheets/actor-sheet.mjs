4b7a32a1f3bc07dc1be4dd4463a096a9dd484a11
madupree <madupree@iastate.edu>
2022-10-12 20:24:06 -0500

    rebuilding, initiative nearly works

--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -1,173 +1,130 @@
-import { EntitySheetHelper } from "../helper.js";
-import {ATTRIBUTE_TYPES} from "../constants.js";
-
 /**
- * Extend the basic ActorSheet with some very simple modifications
+ * Extend the ActorSheet class for SRPG
  * @extends {ActorSheet}
  */
 export class SRPGActorSheet extends ActorSheet {
 
-  /** @inheritdoc */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["srpg", "sheet", "actor"],
-      template: "systems/srpg/templates/actor/actor-sheet.hbs",
-      width: 600,
-      height: 600,
-      tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
-      scrollY: [".biography", ".items", ".attributes"],
-      dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
-    });
-  }
-
-  /* -------------------------------------------- */
-
-  /** @override */
-  get template() {
-    return `systems/srpg/templates/actor/actor-${this.actor.type}-sheet.hbs`
-  }
-
-  /* -------------------------------------------- */
-
-  // derived data that isn't used anywhere but on the character sheet
-  // dont calculate data her that is useful for rolls
-
-  /** @inheritdoc */
-  async getData(options) {
-    const context = await super.getData(options);
-    EntitySheetHelper.getAttributeData(context.data);
-    context.shorthand = !!game.settings.get("srpg", "macroShorthand");
-    context.systemData = context.data.system;
-    context.dtypes = ATTRIBUTE_TYPES;
-    context.biographyHTML = await TextEditor.enrichHTML(context.systemData.biography, {
-      secrets: this.document.isOwner,
-      async: true
-    });
-
-	  // call helper functions to prepare data
-	  context.systemData = this._availableLevels(context.systemData);
-
-
-    return context;
-  }
-
-  // helpers for getData() here
-
-  /**
-   * calculates total levels in skills, then subtracts that from skill level to show total skill levels available
-   */
-  _availableLevels(systemData) {
-	  // calculate total levels in skills
-	  var totalSkills = 0;
-    for (let skill in systemData.skills) {
-      totalSkills += systemData.skills[skill].value;
+    /** @inheritdoc */
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            classes: ["srpg", "sheet", "actor"],
+            template: "systems/srpg/templates/actor/actor-shinobi-sheet.hbs",
+            width: 600,
+            height: 600,
+            tabs: [{ navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "skills" }],
+            //scrollY: [".biography", ".items", ".attributes"],
+            //dragDrop: [{dragSelector: ".item-list .item", dropSelector: null}]
+        });
     }
-	
-	  // set availableLevels
-	  systemData.availableLevels = systemData.skilllevel - totalSkills;
-	  return systemData;
-  }
 
-
-  /* -------------------------------------------- */
-
-  /** @inheritdoc */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    // Everything below here is only needed if the sheet is editable
-    if ( !this.isEditable ) return;
-
-    // Attribute Management
-    html.find(".attributes").on("click", ".attribute-control", EntitySheetHelper.onClickAttributeControl.bind(this));
-    html.find(".groups").on("click", ".group-control", EntitySheetHelper.onClickAttributeGroupControl.bind(this));
-    html.find(".attributes").on("click", "a.attribute-roll", EntitySheetHelper.onAttributeRoll.bind(this));
-
-    // Item Controls
-    html.find(".item-control").click(this._onItemControl.bind(this));
-    html.find(".items .rollable").on("click", this._onItemRoll.bind(this));
-
-	  // my listeners
-	  html.find(".skills .rollable").on("click", this._onSkillRoll.bind(this));
-
-    // Add draggable for Macro creation
-    html.find(".attributes a.attribute-roll").each((i, a) => {
-      a.setAttribute("draggable", true);
-      a.addEventListener("dragstart", ev => {
-        let dragData = ev.currentTarget.dataset;
-        ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-      }, false);
-    });
-  }
-
-  /* -------------------------------------------- */
-
-  /**
-   * Handle click events for Item control buttons within the Actor Sheet
-   * @param event
-   * @private
-   */
-  _onItemControl(event) {
-    event.preventDefault();
-
-    // Obtain event data
-    const button = event.currentTarget;
-    const li = button.closest(".item");
-    const item = this.actor.items.get(li?.dataset.itemId);
-
-    // Handle different actions
-    switch ( button.dataset.action ) {
-      case "create":
-        const cls = getDocumentClass("Item");
-        return cls.create({name: game.i18n.localize("SRPG.ItemNew"), type: "item"}, {parent: this.actor});
-      case "edit":
-        return item.sheet.render(true);
-      case "delete":
-        return item.delete();
+    /** @override */
+    get template() {
+        return `systems/srpg/templates/actor/actor-${this.actor.type}-sheet.hbs`;
     }
-  }
 
-  /* -------------------------------------------- */
+    /**
+     * calculate derived data that isn't used anywhere but on character sheet
+     * don't calculate data for rolls here
+     * @inheritdoc
+     */
+    async getData(options) {
+        const context = await super.getData(options);
+        // shorthand here if necessary
+        context.systemData = context.data.system;
+        //context.biographyHTML = await TextEditor.enrichHTML(context.systemData.biography, {
+        //    secrets: this.document.isOwner,
+        //    async: true
+        //});
 
-  /**
-   * Listen for roll buttons on items.
-   * @param {MouseEvent} event    The originating left click event
-   */
-  _onItemRoll(event) {
-    let button = $(event.currentTarget);
-    const li = button.parents(".item");
-    const item = this.actor.items.get(li.data("itemId"));
-    let r = new Roll(button.data('roll'), this.actor.getRollData());
-    return r.toMessage({
-      user: game.user.id,
-      speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-      flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
-    });
-  }
+        // helper functions
+        return context;
+    }
 
-  /* -------------------------------------------- */
+    /* helper functions for getData() */
+    /**
+     * calculates total skill levels available
+     */
+    _availableLevels(systemData) {
+        // calculate total levels in skills
+        var totalSkills = 0;
+        for (let skill in systemData.skills) {
+            totalSkills += systemData.skills[skill].value;
+        }
 
-  /** @inheritdoc */
-  _getSubmitData(updateData) {
-    let formData = super._getSubmitData(updateData);
-    formData = EntitySheetHelper.updateAttributes(formData, this.object);
-    formData = EntitySheetHelper.updateGroups(formData, this.object);
-    return formData;
-  }
+        // set availableLevels
+        systemData.availableLevels = systemData.skilllevel - totalSkills;
+        return systemData;
+    }
 
-	/* ---------------------------------------------- */
-	/**
-	 * @param {MouseEvent} event 
-	 */
-	_onSkillRoll(event) {
-		event.preventDefault();
-		let button = $(event.currentTarget);
-		let r = new Roll(button.data('roll'), this.actor.getRollData());
-		return r.toMessage({
-			user: game.user.id,
-			speaker: ChatMessage.getSpeaker({ actor: this.actor }),
-			flavor: `<h2 class="skillroll">${button.data('label')}</h2>`
-		})
+    /** @inheritdoc */
+    activateListeners(html) {
+        super.activateListeners(html);
 
-	}
+        // Everything below here is only needed if sheet is editable
+        if (!this.isEditable) { return; }
+
+        // Item Controls
+        html.find(".item-control").click(this._onItemControl.bind(this));
+        html.find(".items .rollable").on("click", this._onItemRoll.bind(this));
+
+        // my listeners
+        html.find(".skills .rollable").on("click", this._onSkillRoll.bind(this));
+
+    }
+
+    /**
+     * Handle click events for Item control buttons within the Actor Sheet
+     * @param event
+     * @private
+     */
+    _onItemControl(event) {
+        event.preventDefault();
+
+        // Obtain event data
+        const button = event.currentTarget;
+        const li = button.closest(".item");
+        const item = this.actor.items.get(li?.dataset.itemId);
+
+        // Handle different actions
+        switch (button.dataset.action) {
+            case "create":
+                const cls = getDocumentClass("Item");
+                return cls.create({ name: game.i18n.localize("SRPG.ItemNew"), type: "item" }, { parent: this.actor });
+            case "edit":
+                return item.sheet.render(true);
+            case "delete":
+                return item.delete();
+        }
+    }
+
+    /**
+     * Listen for roll buttons on items.
+     * @param {MouseEvent} event    The originating left click event
+     */
+    _onItemRoll(event) {
+        let button = $(event.currentTarget);
+        const li = button.parents(".item");
+        const item = this.actor.items.get(li.data("itemId"));
+        let r = new Roll(button.data('roll'), this.actor.getRollData());
+        return r.toMessage({
+            user: game.user.id,
+            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            flavor: `<h2>${item.name}</h2><h3>${button.text()}</h3>`
+        });
+    }
+
+    /**
+     * @param {MouseEvent} event 
+     */
+    _onSkillRoll(event) {
+        event.preventDefault();
+        let button = $(event.currentTarget);
+        let r = new Roll(button.data('roll'), this.actor.getRollData());
+        return r.toMessage({
+            user: game.user.id,
+            speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+            flavor: `<h2 class="skillroll">${button.data('label')}</h2>`
+        })
+
+    }
 }
